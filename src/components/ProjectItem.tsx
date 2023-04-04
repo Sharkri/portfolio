ac77420d62a95d6f69c15a29e700b640225c4083
Sharkri <sharkdude955@gmail.com>
2023-04-04 15:20:55 -0500

    Display three projects

---
+++ src/components/ProjectItem.tsx
@@ -0,0 +1,75 @@
+import Project from "../interfaces/ProjectInterface";
+
+function ProjectItem({ project }: { project: Project }) {
+  return (
+    <div className="text-white flex flex-col gap-8 border-b border-b-zinc-600 mb-7 pb-8">
+      <h1 className="text-xl font-bold">{project.title}</h1>
+
+      <div className="flex max-lg:flex-col gap-6">
+        <div className="flex-[50%] overflow-hidden">
+          <img
+            src={project.image}
+            className="brightness-[0.8] hover:brightness-90 hover:scale-105 transition duration-300"
+            alt=""
+          />
+        </div>
+
+        <div className="flex-[50%] flex flex-col gap-8">
+          <div>
+            <h1 className="text-lg font-bold">Summary</h1>
+            <p className="text-white/75">{project.description}</p>
+          </div>
+
+          <div>
+            <h1 className="text-lg font-bold">Technologies</h1>
+            {project.technologies.map((technologyName, index) => (
+              <span key={technologyName} className="text-white/75">
+                {technologyName}
+                {
+                  // remove trailing comma
+                  index !== project.technologies.length - 1 && ", "
+                }
+              </span>
+            ))}
+          </div>
+
+          <div className="flex gap-6 font-bold">
+            <a
+              href={project.livePreview}
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center gap-3 group"
+            >
+              <span>
+                LIVE PREVIEW
+                <div className="h-0.5 bg-white/75 origin-left translate-y-1 scale-x-0 group-hover:scale-x-100 transition-transform duration-500" />
+              </span>
+              <i
+                className="fa-solid fa-arrow-up rotate-45"
+                aria-label="open link icon"
+              />
+            </a>
+
+            <a
+              href={project.githubRepo}
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center gap-3 group"
+            >
+              <span>
+                VIEW CODE
+                <div className="h-0.5 bg-white/75 origin-left translate-y-1 scale-x-0 group-hover:scale-x-100 transition-transform duration-500" />
+              </span>
+              <i
+                className="fa-solid fa-arrow-up rotate-45"
+                aria-label="open link icon"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectItem;
---
+++ src/interfaces/ProjectInterface.ts
@@ -0,0 +1,8 @@
+export default interface Project {
+  title: string;
+  image: string;
+  description: string;
+  technologies: string[];
+  livePreview: string;
+  githubRepo: string;
+}
