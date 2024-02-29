58650a4fead75b4c622dbb225929b1207c75273e
Sharkri <sharkdude955@gmail.com>
2024-02-29 11:23:54 -0600

    style: make topics less thick

--- src/routes/blog/components/topics.tsx
+++ src/routes/blog/components/topics.tsx
@@ -2,10 +2,10 @@ import { Post } from "../../../types/Post";
 
 export default function Topics({ topics }: { topics: Post["topics"] }) {
   return (
-    <div className="flex flex-wrap gap-3.5 items-end">
+    <div className="flex flex-wrap gap-3 items-end">
       {topics?.map((topic) => (
         <div
-          className="bg-zinc-800 rounded-full hover:brightness-110 transition py-2 px-5 text-sm cursor-pointer"
+          className="bg-zinc-800 rounded-full hover:brightness-110 transition py-1.5 px-3 text-sm cursor-pointer"
           key={topic}
         >
           {topic}
