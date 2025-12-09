289d7b30d61012dc16de48d1f917ddd09b3ad705
Sharkri <sharkdude955@gmail.com>
2025-12-09 00:13:08 -0600

    fix: img wrong path

--- src/routes/blog/components/404.tsx
+++ src/routes/blog/components/404.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
 
       <div className="p-10 flex flex-col gap-6 justify-center items-center">
         <h1 className="text-8xl font-bold">404</h1>
-        <img src="/assets/images/sadcat.gif" alt="" width={250} />
+        <img src="/portfolio/assets/images/sadcat.gif" alt="" width={250} />
         <p className="text-xl text-muted">
           This page doesn&apos;t exist.{" "}
           <Link to="/" className="underline hover:text-foreground text-lg">
