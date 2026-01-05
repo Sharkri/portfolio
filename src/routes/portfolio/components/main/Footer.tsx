077cab1b584f93545f19827466afe9b2b192c053
Sharkri <sharkdude955@gmail.com>
2026-01-05 14:06:06 -0600

    feat(ui): make footer more nicely formatted

--- src/routes/portfolio/components/main/Footer.tsx
+++ src/routes/portfolio/components/main/Footer.tsx
@@ -3,28 +3,27 @@ function Footer() {
     <footer className="pb-8 pt-6 border-slate-700 border-t">
       <div className="flex flex-col items-center">
         <div className="flex flex-col gap-4">
-          <div>
-            <div>
-              <a
-                href="mailto:snowyshark@proton.me"
-                className="underline underline-offset-2 text-muted hover:text-foreground"
-              >
-                contact me :3
-              </a>
-            </div>
-          </div>
-
           <div className="flex justify-center gap-4 text-muted">
-            <div>my portfolio is also open-source!</div>
             <a
               href="https://github.com/sharkri/portfolio"
               target="_blank"
               rel="noreferrer"
-              title="GitHub Repo"
+              className="underline underline-offset-2 hover:text-foreground"
             >
-              <i className="fa-brands fa-github text-2xl" aria-hidden="true" />
-              <span className="sr-only">GitHub Repo Page</span>
+              source code
             </a>
+            ---
+            <a
+              href="mailto:snowyshark@proton.me"
+              className="underline underline-offset-2 hover:text-foreground"
+            >
+              contact me
+            </a>
+          </div>
+
+          <div className="text-muted max-w-sm text-center">
+            coded by me using <span className="font-bold">vite + react</span>{" "}
+            and deployed via github pages.
           </div>
         </div>
       </div>
