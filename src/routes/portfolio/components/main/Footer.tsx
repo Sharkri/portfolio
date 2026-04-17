0d92de5f29a9d9b6341cb11cbfdc608d26ef1f78
Sharkri <sharkdude955@gmail.com>
2026-04-17 16:47:55 -0500

    Add spritesheet thing

--- src/routes/portfolio/components/main/Footer.tsx
+++ src/routes/portfolio/components/main/Footer.tsx
@@ -3,36 +3,26 @@ function Footer() {
     <footer className="pb-8 pt-6 border-slate-700 border-t">
       <div className="flex flex-col items-center">
         <div className="flex flex-col gap-4">
-          <div className="flex justify-center gap-4 text-muted">
+          <div className="flex justify-center items-center gap-4 text-muted">
             <a
               href="https://github.com/sharkri/portfolio"
               target="_blank"
               rel="noreferrer"
-              className="underline underline-offset-2 hover:text-accent hover:text-shadow-lg transition"
+              className="underline underline-offset-2 hover:text-accent-secondary hover:text-shadow-lg transition"
             >
               source code
             </a>
-            ---
+            <div className="border-b h-1 w-6 border-muted" />
             <a
               href="mailto:snowyshark@proton.me"
-              className="underline underline-offset-2 hover:text-accent hover:text-shadow-lg transition"
+              className="underline underline-offset-2 hover:text-accent-secondary hover:text-shadow-lg transition"
             >
               contact me
             </a>
           </div>
-          <div className="text-muted max-w-sm text-center">
-            coded by me using <span className="font-bold">vite + react</span>{" "}
-            and deployed via github pages.
-          </div>
-
-          <div className="flex flex-col items-center pt-10 max-sm:pt-6">
-            <img
-              src="/portfolio/assets/images/dolljirachi1.png"
-              width={64}
-              height={64}
-              alt=""
-            />
-            <p className="text-muted">thank you for visiting &lt;3</p>
+          <div className="flex flex-col items-center pt-6">
+            <div className="layer sprite" aria-hidden="true" />
+            <p className="text-accent-secondary mt-3">fin.</p>
           </div>
         </div>
       </div>
